bd5570755375134b57810b387093d2b8d07f96c5
SantiagoRojasPc <santiagodrp37@gmail.com>
2022-09-30 18:18:50 -0500

    primer commit

---
+++ BackEnd/BackEnd/Migrations/20220930194622_Inical.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackEnd.Migrations
+{
+    public partial class Inical : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Car",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nombre = table.Column<string>(type: "varchar(100)", nullable: false),
+                    matricula = table.Column<string>(type: "varchar(7)", nullable: false),
+                    celular = table.Column<string>(type: "varchar(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Car", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Car");
+        }
+    }
+}
---
+++ BackEnd/BackEnd/Models/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Models
+{
+    public class Car
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public string nombre { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(7)")]
+        public string matricula { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(10)")]
+        public string celular { get; set; }
+
+
+    }
+}
